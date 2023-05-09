5606e36562f0bedb97aa2ed82d101cf89835bf16
Millione <isjieliu@163.com>
2023-05-09 18:00:29 +0800

    add docs for lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,46 @@
+//! This crate provides concurrent-safe typedcache with expiration capabilities.
+//!
+//! ## Usage
+//!
+//! Add this to your `Cargo.toml`:
+//!
+//! ```toml
+//! [build-dependencies]
+//! typedcache = "0.2"
+//! ```
+//!
+//! ## Example
+//! ```rust
+//! use std::time::Duration;
+//!
+//! use typedcache::typed::TypedMap;
+//!
+//! #[tokio::main]
+//! async fn main() {
+//!     let cache = typedcache::cache("test".into());
+//!     cache
+//!         .add(
+//!             TestKey("key_erpired_after_1s".into()),
+//!             Duration::from_secs(1),
+//!             TestValue(1),
+//!         );
+//!     tokio::time::sleep(Duration::from_secs(2)).await;
+//!     assert!(cache
+//!         .get(&TestKey("key_erpired_after_1s".into()))
+//!         .is_none());
+//! }
+//!
+//! #[derive(Debug, Clone, Eq, PartialEq, Hash)]
+//! pub struct TestKey(String);
+//!
+//! impl TypedMap for TestKey {
+//!     type Value = TestValue;
+//! }
+//!
+//! pub struct TestValue(isize);
+//! ```
+//!
+
 pub mod error;
 pub mod item;
 pub mod table;
@@ -13,8 +56,8 @@ lazy_static::lazy_static! {
     pub static ref CACHE: RwLock<HashMap<String, CacheTable>> = RwLock::new(HashMap::new());
 }
 
-// Cache returns the existing cache table with given name or creates a new one
-// if the table does not exist yet.
+/// Cache returns the existing cache table with given name or creates a new one
+/// if the table does not exist yet.
 pub fn cache(name: String) -> CacheTable {
     let cache = CACHE.read().unwrap();
     if let Some(table) = cache.get(&name) {
